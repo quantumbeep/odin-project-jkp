864c399390f14e51a3eef301ef5c07a0ee20763b
quantumbeep <kh@khs-mac-mini.lan>
2022-05-31 14:58:10 -0700

    add html ui buttons for player input

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # odin-project-jkp
 rock paper scissors
+
+notes to self
+1. understand the problem.
+- make a js game that plays rock paper scissors with the computer in browser console
+
+2. plan
+- no UI, strictly in browser console
+- inputs: player selection, computer selection, strings
+- outputs: winner of the round (player or computer), a string message indicating the winner and how it was won
+
+3. pseudo
+- it should take an input from the user of either rock, paper, or scissor in a typed string format
+- the input should only take 1 correct spelling or variation of that string. e.g. rock and not rocks
+- the input should ignore case of input string. e.g. Rock ROCK rOcK are all valid inputs
+- it should calculate a computer selection randomly from the same input strings as the user
+- it should compare the user input and the computer input
+- if the user chose rock and the computer chose scissor, then the user wins
+- if the user chose rock and the computer chose paper, then the computer wins
+- if the user chose rock and the computer chose rock, then it is a tie
+- and so on for the other inputs...
+- finally, it will print the result of the comparison and which player won.
+
+
--- jkp.js
+++ jkp.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let comScore = 0;
+let round = 0;
 
 const computerPlay = () => {
   const num = Math.floor(Math.random() * 3);
@@ -21,48 +22,38 @@ const cleanInput = (playerInput) => {
   }
 };
 
-const playRound = (playerSelection, computerSelection, i) => {
+const playRound = (playerSelection) => {
+  round++;
+  let computerSelection = computerPlay();
+
   console.log(playerSelection, computerSelection);
   if (playerSelection == computerSelection) {
     console.log(
-      `Round ${
-        i + 1
-      } is a tie! Current score: Player ${playerScore} - Com ${comScore}`
+      `Round ${round} is a tie! Current score: Player ${playerScore} - Com ${comScore}`
     );
   } else if (
-    (playerSelection === 'rock' && computerSelection === 'paper') ||
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
     (playerSelection === 'paper' && computerSelection === 'rock') ||
     (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
     playerScore++;
     console.log(
-      `player wins Round ${
-        i + 1
-      } Current score: Player ${playerScore} - Com ${comScore}`
+      `player wins Round ${round} Current score: Player ${playerScore} - Com ${comScore}`
     );
   } else {
     comScore++;
     console.log(
-      `player loses Round ${
-        i + 1
-      } Current score: Player ${playerScore} - Com ${comScore}`
+      `player loses Round ${round} Current score: Player ${playerScore} - Com ${comScore}`
     );
+  }
+
+  if (playerScore === 5) {
+    console.log(`you win! ${playerScore} to ${comScore}`);
+  } else if (comScore === 5) {
+    console.log(`you loser! ${playerScore} to ${comScore}`);
+  } else {
+    return;
   }
 };
 
-const game = () => {
-  for (let i = 0; i < 5; i++) {
-    let playerInput = prompt(
-      'type your choice of rock, paper, or scissors. just like in real life, if your choice is not clear, you lose.'
-    );
-    let playerSelection = cleanInput(playerInput);
-    let computerSelection = computerPlay();
-    console.log(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection, i);
-  }
-  if (playerScore > comScore) {
-    return `you win! ${playerScore} to ${comScore}`;
-  } else {
-    return `you loser! ${playerScore} to ${comScore}`;
-  }
-};
+const game = () => {};
